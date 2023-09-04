defa900525afea82b81402f2d4a7565ed2be977b
John Leo <leo@halfaya.org>
2023-09-03 17:52:20 -0700

    try again

--- EggForm/Semantics.lean
+++ EggForm/Semantics.lean
@@ -6,12 +6,28 @@ namespace Semantics
 open Syntax
 
 inductive FunctionType : Type
-  | N : FunctionType
   | C : FunctionType
+  | N : FunctionType
+  deriving BEq
 
 structure Signature where
   sig::
   i : List FunctionType -- input types
   o : FunctionType      -- output type
+  deriving BEq
 
-def Schema : Type := Std.HashMap String Int
+def Schema : Type := Std.HashMap String Signature
+
+def constToType : Constant → FunctionType
+  | Constant.c _ => FunctionType.C
+  | Constant.n _ => FunctionType.N
+
+def getSignature : FunctionO Constant Constant → Signature
+  | FunctionO.f (Function.f _ ins) out =>
+      Signature.sig (List.map constToType ins) (constToType out)
+
+def validSignature : Schema → FunctionO Constant Constant → Bool
+  | schema, f =>
+      match Std.HashMap.find? schema (functionOName f) with
+      | none => false
+      | some sig => sig == getSignature f
